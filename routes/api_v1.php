be5224c77e2b8f7eae3bd33d63488d2e371b5c14
Andy <andy@shakewell.agency>
2024-11-06 09:21:37 +1000

    Implemented Task and Versioning API

---
+++ routes/api_v1.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
+
+Route::apiResource('tickets', TicketController::class);
