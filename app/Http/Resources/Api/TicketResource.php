cb58874bdab7607bef8d28215b538ace5618744e
Andy <andy@shakewell.agency>
2024-11-06 17:04:37 +1000

    Limiting the resource property to use

--- app/Http/Resources/Api/TicketResource.php
+++ app/Http/Resources/Api/TicketResource.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class TicketResource extends JsonResource
 {
+    // public static $wrap = 'ticket';
+
+
     /**
      * Transform the resource into an array.
      *
@@ -14,6 +17,33 @@ class TicketResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'type' => "ticket",
+            "id" => $this->id,
+            "attributes" => [
+                "title" => $this->title,
+                "description" => $this->when(
+                    $request->routeIs('api_v1tickets.show'),
+                    $this->description
+                ),
+                "status" => $this->status,
+                "createdAt" => $this->created_at,
+                "updatedAt" => $this->updated_at
+            ],
+            "relationships" => [
+                "author" => [
+                    "data" => [
+                        "type" => "user",
+                        "id" => $this->user_id
+                    ],
+                    "links" => [
+                        ["self" => "todo"]
+                    ]
+                ]
+            ],
+            "links" => [
+                ["self" => route('api_v1tickets.show', ['ticket' => $this->id])]
+            ]
+        ];
     }
 }
